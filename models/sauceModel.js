1554e04c21f2ae4749e24df2271483ce51f1d672
cosmonet-fr <stephane.billois@hotmail.com>
2020-06-24 17:06:04 +0200

    File auth.js created and sauces model ok.

---
+++ models/sauceModel.js
@@ -0,0 +1,22 @@
+console.log("run sauceModel.js");
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const sauceSchema({
+  userId: { type: String, required: true},
+  name: { type: String, required: true,  unique:true },
+  manufacturer: { type: String, required: true },
+  description: { type: String },
+  mainPepper: { type: String },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: String, },
+  usersLiked: [{ type: String, required: false }],
+  usersDisliked: [{ type: String, required: false }]
+});
+
+
+sauceSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('Sauces', sauceSchema);
