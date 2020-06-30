525e72419fb48d97476a1efaea9c573fd38d0cf4
cosmonet-fr <stephane.billois@hotmail.com>
2020-06-30 08:52:52 +0200

    Add default value to sauce model. And bug name object OK.

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,9 +1,11 @@
-const saucesModel = require('../models/Sauce.js');
+const Sauce = require('../models/Sauce.js');
 exports.createSauce = (req, res, next) => {
+
+  console.log(req.body);
   const sauce = new Sauce({
 
   });
-  console.log(req.body.sauce);
+  console.log(sauce);
   sauce.save()
   .then(() => res.status(201).json({ message: 'Nouvelle sauce enregistrée dans la base de données !'}))
   .catch(error => res.status(400).json({ error }));
